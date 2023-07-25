0043befacf8c647a1e899d062f0d5c63483293b7
lilia <liliakai@gmail.com>
2023-07-25 15:35:35 +0200

    Add Bellingcat favicon
    Uses the built in webpack asset management system.
    https://webpack.js.org/guides/asset-management/

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,10 @@ const config = {
             },
           }
         },
+        {
+          test: /\.(ico|png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
       ],
     },
     resolve: {
