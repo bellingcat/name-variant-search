8340e706fab0d26d96eb38889d951da9e2797f41
Lilia Kai <liliakai@gmail.com>
2024-01-23 09:32:39 -1000

    Fix UI not rendering

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-let output = document.querySelector("#output");
-const root = createRoot(output);
-root.render( <App />);
+window.onload = function() {
+  let output = document.querySelector("#output");
+  const root = createRoot(output);
+  root.render( <App />);
+};
 
